2014ed30ad52013f821336daf86f4eb9a3af84bb
Krzysztof Kowalczyk <kowalczyk.krzysztof@gmail.com>
2019-02-14 13:03:16 +0000

    Making proper bean

--- wrong-property-reported/src/main/java/micronaut/problems/ClassUsingPropertyA.java
+++ wrong-property-reported/src/main/java/micronaut/problems/ClassUsingPropertyA.java
@@ -2,6 +2,9 @@ package micronaut.problems;
 
 import io.micronaut.context.annotation.Property;
 
+import javax.inject.Singleton;
+
+@Singleton // singleton is needed to make it a proper bean
 public class ClassUsingPropertyA {
 
     @Property(name = "property.a")
--- wrong-property-reported/src/main/java/micronaut/problems/ScheduledTask.java
+++ wrong-property-reported/src/main/java/micronaut/problems/ScheduledTask.java
@@ -1,10 +1,12 @@
 package micronaut.problems;
 
+import io.micronaut.context.annotation.Parallel;
 import io.micronaut.scheduling.annotation.Scheduled;
 
 public class ScheduledTask {
     static int counter = 0;
 
+    //@Parallel(shutdownOnError = false) // this would fix all tests
     @Scheduled(fixedRate = "${property.b}")
     void task() {
         counter++;
